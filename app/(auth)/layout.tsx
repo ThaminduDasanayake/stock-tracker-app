e43b4dfbf937517e2cf385192aa80e190e2fce29
Thamindu Dasanayake <thamindudasanayake@gmail.com>
2025-10-01 22:20:04 +0530

    Implement auth logic

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,21 @@
+import { auth } from "@/lib/better-auth/auth";
+import { headers } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (session?.user) redirect("/");
+
   return (
     <main className="auth-layout">
       <section className="auth-left-section scrollbar-hide-default">
         <Link href="/" className="auth-logo">
           <Image
             src="/assets/icons/logo.svg"
-            alt="Stock Pulse logo"
+            alt="Signalist logo"
             width={140}
             height={32}
             className="h-8 w-auto"
@@ -20,7 +27,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <section className="auth-right-section">
         <div className="z-10 relative lg:mt-4 lg:mb-16">
           <blockquote className="auth-blockquote">
-            Stock Pulse turned my watchlist into a winning list. The alerts are
+            Signalist turned my watchlist into a winning list. The alerts are
             spot-on, and I feel more confident making moves in the market
           </blockquote>
           <div className="flex items-center justify-between">
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 import NavItems from "./NavItems";
 import UserDropdown from "./UserDropdown";
 
-const Header = () => {
+const Header = ({ user }: { user: User }) => {
   return (
     <header className="sticky top-0 header">
       <div className="container header-wrapper">
         <Link href="/">
           <Image
             src="/assets/icons/logo.svg"
-            alt="Stock Pulse logo"
+            alt="Signalist logo"
             width={140}
             height={32}
             className="h-8 w-auto cursor-pointer"
@@ -19,7 +19,7 @@ const Header = () => {
         <nav className="hidden sm:block">
           <NavItems />
         </nav>
-        <UserDropdown />
+        <UserDropdown user={user} />
       </div>
     </header>
   );
